258d6a529e03224c5ab9cf47fbbfecc3892dab51
Rodger <rodgef101@gmail.com>
2023-04-16 22:00:40 +0200

    fixed link issue

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 ### route that takes in a path param
 @app.route('/<path:path>')
 def catch_all(path):
-    return get_answer(f"generate an html page containing info related to  {path} also generate links to related topics link should be on the same domain and use the name of the topic")
+    return get_answer(f"generate an html page containing info related to  {path} also generate links to related topics link should be a relative link and use the name of the topic")
 
 if __name__ == '__main__':
     app.run(debug=True)
